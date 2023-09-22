d2c8f352117c670a7e59ac3a649f1726dec8de0d
Jaewoo <jaewoocho1017@gmail.com>
2023-09-22 12:07:11 -0700

    it now opens up the chrome window when opened-tab is clicked

--- popup.js
+++ popup.js
@@ -1,25 +1,70 @@
 // popup.js
 
-// Function to populate the grid with tabs
-function populateTabs() {
-	chrome.tabs.query({}, function (tabs) {
-		const tabGrid = document.getElementById("tabGrid");
-		tabGrid.innerHTML = ""; // Clear existing content
+function extractDomain(url) {
+	const domainRegex = /^https?:\/\/(www\.)?([^/]+)/;
+	const matches = url.match(domainRegex);
+	if (matches && matches.length > 2) {
+		return matches[2];
+	}
+	return url;
+}
 
-		tabs.forEach(function (tab) {
-			const tabItem = document.createElement("div");
-			tabItem.classList.add("tab-item");
-			tabItem.textContent = tab.title;
+function populateTabsByWindow() {
+	const boundaryGrid = document.getElementById("boundaryGrid"); // Updated element ID
+	boundaryGrid.innerHTML = "";
 
-			// Open the tab when clicked
-			tabItem.addEventListener("click", function () {
-				chrome.tabs.update(tab.id, { active: true });
+	chrome.windows.getAll({ populate: true }, function (windows) {
+		windows.forEach(function (window) {
+			const windowDiv = document.createElement("div");
+			windowDiv.classList.add("window"); // Updated class name
+
+			const tabGrid = document.createElement("div");
+			tabGrid.classList.add("tab-grid");
+
+			const windowTitle = document.createElement("div");
+			windowTitle.classList.add("window-title");
+			windowTitle.textContent = window.title || "No Title";
+			// have to extract the name of the websites from the dom
+
+			// then add it to the window Title
+			windowDiv.appendChild(windowTitle);
+
+			window.tabs.forEach(function (tab) {
+				const tabItem = document.createElement("div");
+				tabItem.classList.add("tab-item");
+
+				const tabIcon = document.createElement("img");
+				tabIcon.src = tab.favIconUrl || "default-icon.png"; // Use a default icon if no favicon is available
+				tabIcon.classList.add("tab-icon");
+
+				// Create a span for the tab title
+				const tabTitle = document.createElement("span");
+				tabTitle.textContent = extractDomain(tab.url);
+
+				tabItem.appendChild(tabIcon);
+				tabItem.appendChild(tabTitle);
+
+				// Open the tab when clicked
+				tabItem.addEventListener("click", function () {
+					chrome.tabs.update(
+						tab.id,
+						{ active: true },
+						function (updatedTab) {
+							chrome.windows.update(updatedTab.windowId, {
+								focused: true,
+							});
+						}
+					);
+				});
+
+				tabGrid.appendChild(tabItem);
 			});
 
-			tabGrid.appendChild(tabItem);
+			windowDiv.appendChild(tabGrid);
+			boundaryGrid.appendChild(windowDiv); // Updated parent element
 		});
 	});
 }
 
 // Populate the grid when the popup is loaded
-document.addEventListener("DOMContentLoaded", populateTabs);
+document.addEventListener("DOMContentLoaded", populateTabsByWindow);
